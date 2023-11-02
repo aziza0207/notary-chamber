b853632bc0a65990fd53e1f9d1e778c4ac156fef
valentine <mokrousov.va@yandex.ru>
2023-11-02 13:18:21 +0600

    merged with gh

--- backend/content/serializers/__init__.py
+++ backend/content/serializers/__init__.py
@@ -1,3 +1,5 @@
 from .news_serializer import NewsListSerializer, NewsDetailSerializer
 from .document_serializer import DocumentSerializer,CategorySerializer
 from .faq_serializer import *
+from .useful_links_serializer import *
+from .gallery_serializer import *
--- backend/content/urls.py
+++ backend/content/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import FAQListAPIView, NewsListAPIView, NewsDetailAPIView, NewsPinnedAPIView, DocumentListAPIView
+from .views import FAQListAPIView, NewsListAPIView, NewsDetailAPIView, NewsPinnedAPIView, DocumentListAPIView, LinksListAPIView, PhotoListAPIView, VideoListAPIView
 
 urlpatterns = [
     path('documents/', DocumentListAPIView.as_view(), name='documents'),
     path('faq/', FAQListAPIView.as_view(), name='faq'),
     path('news/', NewsListAPIView.as_view(), name='news'),
     path('news-detail/<str:slug>/', NewsDetailAPIView.as_view(), name='news-detail'),
-    path('news-pinned/', NewsPinnedAPIView.as_view(), name='pinned-news')
-
+    path('news-pinned/', NewsPinnedAPIView.as_view(), name='pinned-news'),
+    path('links/', LinksListAPIView.as_view(), name='links'),
+    path('gallery-photo/', PhotoListAPIView.as_view(), name='gallery-photo'),
+    path('gallery-video/', VideoListAPIView.as_view(), name='gallery-video'),
 ]
