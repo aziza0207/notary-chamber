cbe03ded9873062d5251498131caa375208dafcd
valentine <mokrousov.va@yandex.ru>
2023-12-04 15:48:52 +0600

    added admin site translations

--- backend/ed_centres/apps.py
+++ backend/ed_centres/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class EdCentresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ed_centres'
+    verbose_name = _('Ed_centres')
--- backend/notary/apps.py
+++ backend/notary/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class NotaryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notary'
+    verbose_name = _('Notary')
 
     def ready(self) -> None:
         import notary.signals
