9431439f3e7ea6df7f13cb9504e281fb13f1ecf1
valentine <mokrousov.va@yandex.ru>
2023-12-07 15:07:11 +0600

    deleted ministy url

--- backend/notary/urls.py
+++ backend/notary/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('notary/', NotaryListAPIView.as_view(), name='notary'),
     path('notary-status/', NotaryStatusListView.as_view(), name='notary-status'),
     path('notary-flows/', NotaryFlowListAPIView.as_view(), name='notary-flows'),
-    path('ministry-flows/', MinistryFlowListAPIView.as_view(), name='ministry-flows'),
     path('mail/', make_and_send_message, name='mail')
     ]
